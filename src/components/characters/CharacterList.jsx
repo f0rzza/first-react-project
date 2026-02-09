004142db077fe6954adf431a614374c4b005a2ea
Florent <forzza.dev@gmail.com>
2026-02-09 18:12:50 +0100

    feat: ajoute un composant liste de personnages SW (avec Api)

---
+++ src/components/characters/CharacterList.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react';
+
+export function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  const [count, setCount] = useState(0);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // Update the counter after click on the button.
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  // After each counter update,
+  useEffect(() => {
+    if (count === 0) {
+      return;
+    }
+
+    // Asynchronous function to get SW character details with an API.
+    async function fetchData() {
+      try {
+        const response = await fetch(`https://swapi.info/api/people/${count}`);
+
+        if (!response.ok) {
+          setErrorMessage(`An error has occurred.`);
+          return;
+        }
+
+        // Keep only some details for test.
+        const result = await response.json();
+        const { name, gender } = result;
+
+        // eslint warning : when we use directly 'characters' const.
+        // const newCharacters = [...characters, { name, gender }];
+        // setCharacters(newCharacters);
+
+        // Cleaner and allows to avoid eslint warning in the dependencies.
+        // Note : use 'count' as ID because there are not character ID in the API.
+        setCharacters((prevCharacters) => [...prevCharacters, { id: count, name, gender }]);
+      } catch (error) {
+        console.log(error);
+        setErrorMessage(`An error has occurred.`);
+      }
+    }
+
+    fetchData();
+  }, [count]);
+
+  return (
+    <>
+      <button onClick={handleClick}>Get a SW character</button>
+
+      {/* Display error message if necessary */}
+      {errorMessage && <p>{errorMessage}</p>}
+
+      {/* Display list of characters */}
+      <p>List of characters :</p>
+      {characters.length ? (
+        <ul>
+          {characters.map((c) => (
+            <li key={c.id}>{c.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No characters</p>
+      )}
+
+      {/* Display the counter */}
+      <p>Number of characters: {count}</p>
+    </>
+  );
+}
