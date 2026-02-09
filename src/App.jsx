9d86d466a57f1488853ce44feb0ffb1b5c5ce407
Florent <forzza.dev@gmail.com>
2026-02-09 17:15:24 +0100

    style: déplace la liste des alertes dans une section

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,13 @@ import AlertList from './components/alerts/AlertList';
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <AlertList />
+      <h1>Vite + React - DEMO</h1>
+
+      {/* Alerts */}
+      <section>
+        <h2>Composant Alert + liste</h2>
+        <AlertList />
+      </section>
     </>
   );
 }
