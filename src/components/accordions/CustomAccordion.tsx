d4ae73b6d62d141d487285e672a3a90422157acc
Florent <forzza.dev@gmail.com>
2026-02-12 18:14:27 +0100

    feat: affiche les différents composants dans des accordéons

---
+++ src/components/accordions/CustomAccordion.tsx
@@ -0,0 +1,30 @@
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { ReactNode } from 'react';
+
+type Props = {
+  id: number;
+  title: string;
+  children: ReactNode;
+  defaultExpanded: boolean;
+};
+
+export function CustomAccordion(props: Props) {
+  const { id, title, children, defaultExpanded = false } = props;
+
+  return (
+    <Accordion defaultExpanded={defaultExpanded}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls={`panel${id}-content`}
+        id={`panel${id}-header`}
+      >
+        <Typography component="span">{title}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>{children}</AccordionDetails>
+    </Accordion>
+  );
+}
