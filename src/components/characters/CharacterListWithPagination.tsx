761c4e887472d47576de3714833084c9e05242ee
Florent <forzza.dev@gmail.com>
2026-02-18 22:31:43 +0100

    feat: ajoute le composant Pagination à la liste des personnages

--- src/components/characters/CharacterListWithPagination.tsx
+++ src/components/characters/CharacterListWithPagination.tsx
@@ -1,68 +1,53 @@
-import { JSX, useEffect, useMemo, useReducer } from 'react';
+import { useEffect, useMemo, useReducer } from 'react';
+import { Pagination } from '../common/Pagination';
 
 type State = {
   characters: Array<Object>;
   page: number;
-  totalPages: number;
   isLoading: boolean;
   message: string;
 };
 
 type Action =
   | { type: 'page'; page: number }
-  | { type: 'success'; characters: Array<Object>; totalPages: number }
+  | { type: 'success'; characters: Array<Object> }
   | { type: 'error'; message: string };
 
 function reducer(state: State, action: Action) {
   switch (action.type) {
     case 'page':
       return { ...state, page: action.page };
+
     case 'success':
       return {
         ...state,
         characters: action.characters,
-        totalPages: action.totalPages,
         isLoading: false,
         message: '',
       };
+
     case 'error':
       return { ...state, isLoading: false, message: action.message };
+
     default:
       return state;
   }
 }
 
 // Get characters of the current page.
-function getFilteredCharacters(state: State, nbPerPage: number = 10) {
-  const indexMin = nbPerPage * (state.page - 1);
+function getFilteredCharacters(page: number, characters: Array<Object>, nbPerPage: number = 10) {
+  const indexMin = nbPerPage * (page - 1);
   const indexMax = indexMin + 9;
 
-  return state.characters.filter((p, i) => {
+  return characters.filter((p, i) => {
     if (indexMin <= i && i <= indexMax) return p;
   });
-}
-
-// Get pagination : array of buttons.
-function getPagination(totalPages: number, dispatch: (action: Action) => void): Array<JSX.Element> {
-  const pagination = [];
-
-  for (let index = 1; index <= totalPages; index++) {
-    const button = (
-      <button key={index} onClick={() => dispatch({ type: 'page', page: index })}>
-        {index}
-      </button>
-    );
-    pagination.push(button);
-  }
-
-  return pagination;
 }
 
 export function CharacterListWithPagination() {
   const [state, dispatch] = useReducer(reducer, {
     characters: [],
     page: 1,
-    totalPages: 1,
     isLoading: true,
     message: '',
   });
@@ -87,10 +72,7 @@ export function CharacterListWithPagination() {
           return;
         }
 
-        // Get the number of total pages.
-        const totalPages = Math.ceil(result.length / 10);
-
-        dispatch({ type: 'success', characters: result, totalPages: totalPages });
+        dispatch({ type: 'success', characters: result });
       } catch (error) {
         dispatch({ type: 'error', message: `An error has occurred.-3` });
       }
@@ -99,21 +81,17 @@ export function CharacterListWithPagination() {
     fetchData();
   }, []);
 
+  const { characters, page, isLoading, message } = state;
+
   // When characters list or current page changes, is updated or the current page changes,
   const filteredCharacters = useMemo(
-    () => getFilteredCharacters(state),
-    [state.characters, state.page],
+    () => getFilteredCharacters(page, characters),
+    [characters, page],
   );
-
-  const { page, totalPages, isLoading, message } = state;
-
-  const pagination = getPagination(totalPages, dispatch);
 
   return (
     <div>
-      <p>
-        Liste - {page}/{totalPages}
-      </p>
+      <p>Liste</p>
 
       {isLoading && <p>Loading...</p>}
 
@@ -129,7 +107,11 @@ export function CharacterListWithPagination() {
         </ul>
       )}
 
-      <div>{pagination}</div>
+      <Pagination
+        data={characters}
+        currentPage={page}
+        onPageChange={(page: number) => dispatch({ type: 'page', page })}
+      />
     </div>
   );
 }
