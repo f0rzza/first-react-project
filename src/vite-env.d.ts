4185930331a7166582fbf3cddaeb3d703160c3aa
Florent <forzza.dev@gmail.com>
2026-02-28 03:54:34 +0100

    chore: ajoute une variable d'environnement en lecture

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ViteTypeOptions {
 interface ImportMetaEnv {
   readonly VITE_BASE_SW_API_URL: string;
   readonly VITE_BASE_BLOG_API_URL: string;
+  readonly VITE_NB_POSTS_PER_PAGE: number;
 }
 
 interface ImportMeta {
