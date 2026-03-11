912554fd68d8bb9efea65f71213bdbe8da52a378
Florent <forzza.dev@gmail.com>
2026-03-11 15:49:54 +0100

    feat: ajoute la déconnexion sur le bouton logout

--- src/components/buttons/LogoutButton.tsx
+++ src/components/buttons/LogoutButton.tsx
@@ -1,4 +1,44 @@
-export function LogoutButton() {
+import { useState } from 'react';
+
+type Props = {
+  onClick: () => void;
+};
+
+export function LogoutButton({ onClick }: Props) {
   // TODO : use MUI button
-  return <button>Logout</button>;
+  const [isEnabled, setIsEnabled] = useState(true);
+
+  async function handleClick() {
+    if (!isEnabled) return;
+
+    setIsEnabled(false);
+
+    try {
+      // Call API request
+      const response = await fetch(`${import.meta.env.VITE_BASE_BLOG_API_URL}/auth/logout`, {
+        method: 'POST',
+      });
+
+      if (!response.ok) {
+        return;
+      }
+
+      // Read the body of the response
+      const data = await response.json();
+
+      if (data.success) {
+        onClick();
+      }
+    } catch (error) {
+      console.log('ERROR', error);
+    } finally {
+      setIsEnabled(true);
+    }
+  }
+
+  return (
+    <button onClick={handleClick} disabled={!isEnabled}>
+      Logout
+    </button>
+  );
 }
