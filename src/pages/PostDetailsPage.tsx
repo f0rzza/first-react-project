2d618c237d02b3b4b39c91376545cbef15f6fe68
Florent <forzza.dev@gmail.com>
2026-03-18 20:35:57 +0100

    feat: ajoute quelques pages utilisant le layout

---
+++ src/pages/PostDetailsPage.tsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom';
+import { Layout } from '../layouts/Layout';
+
+export function PostDetailsPage() {
+  const { id } = useParams();
+
+  return (
+    <Layout>
+      <p>Post n°{id} - Details</p>
+    </Layout>
+  );
+}
