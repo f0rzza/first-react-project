d7a9155009ce43a82a6496326ec5d57a5afea647
Florent <forzza.dev@gmail.com>
2026-03-18 18:58:36 +0100

    chore: déplace l'import du contexte dans le fichier principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css';
 import { CustomAccordionList } from './components/accordions/CustomAccordionList';
 import { demoAccordions } from './config/demoAccordions';
-import { AuthProvider } from './contexts/AuthProvider';
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <h1>Vite + React - DEMO</h1>
-        <CustomAccordionList data={demoAccordions} />
-      </AuthProvider>
+      <h1>Vite + React - DEMO</h1>
+      <CustomAccordionList data={demoAccordions} />
     </>
   );
 }
