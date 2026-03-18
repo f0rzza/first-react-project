8c1d7eef933d9895c0106b5cddf5df2e0fab6e13
Florent <forzza.dev@gmail.com>
2026-03-18 20:39:07 +0100

    feat: ajoute le router des pages dans le fichier principal

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
 import { AuthProvider } from './contexts/AuthProvider';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>,
 );
