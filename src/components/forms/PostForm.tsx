a976d1e0ccdd9131447d1c14f95a2d60f7baa93c
Florent <forzza.dev@gmail.com>
2026-03-20 18:23:44 +0100

    feat: pré-remplit le formulaire de la page d'édition

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -1,15 +1,33 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { PostType } from '../../types/common';
 
 type Props = {
-  postData?: PostType;
+  postId: string | undefined;
 };
 
-export function PostForm({ postData }: Props) {
+export function PostForm({ postId }: Props) {
   const [formData, setFormData] = useState<PostType>({
-    title: postData?.title || '',
-    content: postData?.content || '',
+    title: '',
+    content: '',
   });
+
+  useEffect(() => {
+    if (postId) {
+      async function fetchData(id: string) {
+        const response = await fetch(`${import.meta.env.VITE_BASE_BLOG_API_URL}/posts/${id}`);
+
+        if (!response.ok) {
+          console.log('api error');
+          return;
+        }
+
+        const result = await response.json();
+        setFormData(result);
+      }
+
+      fetchData(postId);
+    }
+  }, [postId]);
 
   function handleChange(e: ChangeEvent) {
     const target = e.target as HTMLInputElement | HTMLTextAreaElement;
--- src/pages/PostFormPage.tsx
+++ src/pages/PostFormPage.tsx
@@ -4,13 +4,11 @@ import { Layout } from '../layouts/Layout';
 
 export function PostFormPage() {
   const { id } = useParams();
-  const data = id ? { title: 'mon titre', content: 'mon contenu' } : undefined;
-  // TODO : get post data from ID.
 
   return (
     <Layout>
       <h1>{id ? 'Edit post' : 'Create post'}</h1>
-      <PostForm postData={data} />
+      <PostForm postId={id} />
     </Layout>
   );
 }
