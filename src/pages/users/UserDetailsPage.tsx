4f201bd6b5b79e60a1ae0f955102c048cd25d9e5
Florent <forzza.dev@gmail.com>
2026-03-26 18:44:21 +0100

    feat: ajoute la récupération de l'utilisateur sur la page détails

--- src/pages/users/UserDetailsPage.tsx
+++ src/pages/users/UserDetailsPage.tsx
@@ -1,13 +1,32 @@
 import { useParams } from 'react-router-dom';
 import { Layout } from '../../layouts/Layout';
+import { useQuery } from '@tanstack/react-query';
+import { fetchUser } from '../../utils/api';
 
 export function UserDetailsPage() {
   // Get ID from URL parameters.
   const { id = '' } = useParams();
 
+  // Get cached data or fetch them.
+  const { data, isError, isPending } = useQuery({
+    queryKey: ['user', id],
+    queryFn: () => fetchUser(id),
+    enabled: !!id, // fetch user only if id is defined
+    retry: false, // TODO : retry query when error is not a 404
+  });
+  // Note : cannot use 'error' const from Hook because the fetch function return a 'Response', not an Error. (message and stack are empty)
+
   return (
     <Layout>
-      <p>User n°{id} - Details</p>
+      {isPending && <p>Loading...</p>}
+
+      {isError && <p>User Not Found</p>}
+
+      {data && (
+        <p>
+          User n°{data.id} - {data.username} - Details
+        </p>
+      )}
     </Layout>
   );
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,19 @@
-import { CategoryType } from '../types/common';
+import { CategoryType, UserType } from '../types/common';
 
 // Get category from ID.
 export async function fetchCategory(id: string): Promise<CategoryType | null> {
   const response = await fetch(`${import.meta.env.VITE_BASE_BLOG_API_URL}/categories/${id}`);
+
+  if (!response.ok) {
+    throw new Response('Not Found', { status: 404 });
+  }
+
+  const result = await response.json();
+  return result;
+}
+
+export async function fetchUser(id: string): Promise<UserType> {
+  const response = await fetch(`${import.meta.env.VITE_BASE_BLOG_API_URL}/users/${id}`);
 
   if (!response.ok) {
     throw new Response('Not Found', { status: 404 });
