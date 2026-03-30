4525861bea24d7d8f80c1def0ef7c44932471894
Florent <forzza.dev@gmail.com>
2026-03-30 22:37:55 +0200

    feat: met à jour des contenus de pages (home, posts)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,23 @@
 import { Layout } from '../layouts/Layout';
 import { CustomAccordionList } from '../components/accordions/CustomAccordionList';
 import { demoAccordions } from '../config/demoAccordions';
+import { Link } from 'react-router-dom';
 
 export function HomePage() {
   return (
     <Layout>
       <h1>Vite + React - DEMO</h1>
+
+      <h2>List of demos</h2>
       <CustomAccordionList data={demoAccordions} />
+
+      <div>
+        <a href="http://localhost:5173/users/1">User1</a>
+        <br />
+        <Link to="/users/1">See User 1</Link>
+        <br />
+        <Link to="/users/1">See User 2</Link>
+      </div>
     </Layout>
   );
 }
